b41571f8a67036fdf66d0b3efb8961fd0db081ed
Meris <merisstupar11@gmail.com>
2022-12-25 16:10:36 +0100

    Fulfill fist test with basic UNIT test

--- cypress/e2e/fist_test.cy.js
+++ cypress/e2e/fist_test.cy.js
@@ -1,9 +1,36 @@
-///<reference types="Cypress"/>
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let divide = (a, b) => a / b;
+let multiply = (a, b) => a * b;
 
-describe("First test", () => {
-  beforeEach(() => {
-    cy.visit("/sign-up");
+describe("Unit testing of our app", () => {
+  context("Math POSITIVE number", () => {
+    it("Should add a positive nubmer", () => {
+      expect(add(2, 2)).to.eq(4);
+    });
+
+    it("Should substract a positive nubmer", () => {
+      expect(subtract(3, 3)).to.eq(0);
+    });
+
+    it("Should divide a positive nubmer", () => {
+      expect(divide(2, 2)).to.eq(1);
+    });
+
+    it("Should multiply a positive nubmer", () => {
+      expect(multiply(2, 2)).to.eq(4);
+    });
   });
+});
 
-  it("Basic test", () => {});
+describe("Unit test for DECIMAL numbers", () => {
+  context("Math DECIMAL number", () => {
+    it("Should add a positive nubmer", () => {
+      expect(add(2.2, 2.2)).to.eq(4.4);
+    });
+
+    it("Should substract a positive nubmer", () => {
+      expect(subtract(3.3, 3.3)).to.eq(0);
+    });
+  });
 });
