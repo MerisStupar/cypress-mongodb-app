2d3f1f786cf95af5b7831a56b70da97adf504a14
Meris <merisstupar11@gmail.com>
2023-01-11 23:41:30 +0100

    Small changes to APITest

--- cypress/e2e/API-Testing/APITestPosts.cy.js
+++ cypress/e2e/API-Testing/APITestPosts.cy.js
@@ -2,7 +2,7 @@
 
 describe("API Testing", () => {
   beforeEach(() => {
-    cy.request("http://localhost:3000/api/posts?limit=10");
+    cy.request("GET", "http://localhost:3000/api/posts?limit=10").as("posts");
   });
 
   it("Body Length - Posts Test", () => {
@@ -22,5 +22,65 @@ describe("API Testing", () => {
       .its("headers.content-type")
       .should("include", "application/json")
       .and("include", "charset=utf-8");
+  });
+
+  const apiItems = [
+    {
+      _id: "63ac4b9b26a2f1351aaeb7ed",
+      content: "Test 2",
+      creatorId: "63a219e455a7e3536097e9e2",
+      createdAt: "2022-12-28T13:58:51.297Z",
+      creator: {
+        _id: "63a219e455a7e3536097e9e2",
+        profilePicture: null,
+        name: "Stupar",
+        username: "meris",
+        bio: "",
+      },
+    },
+    {
+      _id: "63ac4b3e26a2f1351aaeb7ec",
+      content: "Test 1",
+      creatorId: "63a219e455a7e3536097e9e2",
+      createdAt: "2022-12-28T13:57:18.985Z",
+      creator: {
+        _id: "63a219e455a7e3536097e9e2",
+        profilePicture: null,
+        name: "Stupar",
+        username: "meris",
+        bio: "",
+      },
+    },
+  ];
+
+  it("Initial posts from END Point- Test", () => {
+    cy.request("GET", "http://localhost:3000/api/posts?limit=10")
+      .its("body.posts")
+      .should("deep.equal", apiItems);
+  });
+
+  /* it("JSON Schema Check", () => {
+    cy.request("GET", "http://localhost:3000/api/posts?limit=10")
+      .its("body.posts")
+      .each((value) => {
+        expect(value).to.have.all.keys(
+          "_id",
+          "content",
+          "creatorId",
+          "createdAt",
+          "_id",
+          "profilePicture",
+          "name",
+          "username",
+          "bio"
+        );
+      });
+  }); */
+
+  it("Using Alias Request", () => {
+    cy.get("@posts").should((response) => {
+      expect(response.body.posts).to.have.length(2);
+      expect(response).to.have.property("headers");
+    });
   });
 });
